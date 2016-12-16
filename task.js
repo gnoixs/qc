c9b139005fe5c57940b89ef821823911fd503da1
gnoixs <gnoixs@outlook.com>
2016-12-16 19:46:02 +0800

    task

--- task.js
+++ task.js
@@ -4,6 +4,6 @@ var childp = require('child_process');
 //秒 分 时 日 月 周
 var task = new cronJob('0 55 23 * * *',function(){
 	var task = childp.spawn(process.execPath,['app.js']);
-} null, true, 'Asia/Chongqing');
+}, null, true, 'Asia/Chongqing');
 
 task.start();
